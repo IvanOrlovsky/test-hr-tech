55dec8909782c81ba6f2d8577fa2cde6cb3abf91
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-09-13 15:27:47 +0300

    Идеальная навигация

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,37 +1,116 @@
-const NavigationBar: React.FC = () => (
-	<nav className="bg-white shadow px-8 py-4 flex justify-between items-center">
-		<div className="text-xl font-semibold">HarmonyHR</div>
-		<div className="flex gap-4">
-			<a href="#" className="hover:underline">
-				Home
-			</a>
-			<a href="#" className="hover:underline">
-				My Info
-			</a>
-			<a href="#" className="hover:underline">
-				People
-			</a>
-			<a href="#" className="hover:underline">
-				Hiring
-			</a>
-			<a href="#" className="hover:underline">
-				Reports
-			</a>
-			<a href="#" className="hover:underline">
-				Files
-			</a>
-		</div>
-		<div className="flex items-center gap-4">
-			<button className="border border-black px-4 py-2 rounded-xl">
-				Search
-			</button>
-			<img
-				className="w-10 h-10 rounded-full"
-				src="https://via.placeholder.com/38x38"
-				alt="Profile"
-			/>
-		</div>
-	</nav>
-);
+"use client";
 
+import { IoIosSearch } from "react-icons/io";
+
+import { useState } from "react";
+import { cn } from "@/lib/cn";
+
+const NavigationBar = () => {
+	const [activeLink, setActiveLink] = useState("");
+	return (
+		<nav className="bg-white  px-8 flex flex-row items-center justify-between">
+			<div className="flex flex-row gap-8">
+				<div className="text-xl font-semibold self-center my-4">
+					HarmonyHR
+				</div>
+				<div className="flex flex-row gap-4 mt-4 self-end">
+					<a
+						href="#"
+						onClick={() => setActiveLink("Home")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md  p-3 font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "Home",
+							}
+						)}
+					>
+						Home
+					</a>
+					<a
+						href="#"
+						onClick={() => setActiveLink("My Info")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md p-3   font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "My Info",
+							}
+						)}
+					>
+						My Info
+					</a>
+					<a
+						href="#"
+						onClick={() => setActiveLink("People")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md p-3   font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "People",
+							}
+						)}
+					>
+						People
+					</a>
+					<a
+						href="#"
+						onClick={() => setActiveLink("Hiring")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md p-3   font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "Hiring",
+							}
+						)}
+					>
+						Hiring
+					</a>
+					<a
+						href="#"
+						onClick={() => setActiveLink("Reports")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md  p-3  font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "Reports",
+							}
+						)}
+					>
+						Reports
+					</a>
+					<a
+						href="#"
+						onClick={() => setActiveLink("Files")}
+						className={cn(
+							" hover:bg-[#DAE6F2] hover:rounded-t-md p-3 font-semibold",
+							{
+								"bg-[#DAE6F2] rounded-t-md":
+									activeLink === "Files",
+							}
+						)}
+					>
+						Files
+					</a>
+				</div>
+			</div>
+			<div className="flex items-center gap-4 self-center">
+				<div className="border border-black px-4 py-2 rounded-xl flex flex-row gap-2">
+					<IoIosSearch className="text-black self-center" />
+
+					<input
+						type="search"
+						placeholder="Search"
+						className="outline-none"
+					></input>
+				</div>
+				<img
+					className="w-10 h-10 rounded-full"
+					src="https://via.placeholder.com/38x38"
+					alt="Profile"
+				/>
+			</div>
+		</nav>
+	);
+};
 export default NavigationBar;
