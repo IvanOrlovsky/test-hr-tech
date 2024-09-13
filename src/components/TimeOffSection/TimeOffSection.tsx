ef26c8ab2f8141cff15c4a84042f00a0287d21fc
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-09-13 15:53:19 +0300

    Нормально сделал секцию, пойдет

--- src/components/TimeOffSection/TimeOffSection.tsx
+++ src/components/TimeOffSection/TimeOffSection.tsx
@@ -1,30 +1,58 @@
 import React from "react";
 
+import { CiMedicalCross } from "react-icons/ci";
+import { PiMountains } from "react-icons/pi";
+import { GrDocumentTime } from "react-icons/gr";
+
 const TimeOffSection = () => {
 	const timeOffData = [
-		{ type: "Sick", daysAvailable: 3, scheduledText: "1 day scheduled" },
-		{ type: "Annual Leave", daysAvailable: 10.3, scheduledText: "" },
+		{
+			type: "Sick",
+			icon: <CiMedicalCross className="self-center" />,
+			daysAvailable: 3,
+			unitOfMeasurement: "Days Available",
+			scheduledText: "1 day scheduled",
+			underText: "Sick Full-Time",
+		},
+		{
+			type: "Annual Leave",
+			icon: <PiMountains className="self-center" />,
+			daysAvailable: 10.3,
+			unitOfMeasurement: "Days Available",
+			scheduledText: "",
+			underText: "Holiday Full-Time",
+		},
 		{
 			type: "Comp/In Lieu Time",
+			icon: <GrDocumentTime className="self-center" />,
 			daysAvailable: 0,
-			scheduledText: "Human Used(YTD)",
+			unitOfMeasurement: "Human Used(YTD)",
+			scheduledText: "",
+			underText: "Comp/in Lieu Time Flexible Policy",
 		},
 	];
 
 	return (
 		<section>
-			<h2 className="text-xl font-semibold mb-4">Time Off</h2>
-			<div className="flex flex-row gap-4 ">
+			<div className="flex flex-row gap-2 pb-2 mb-4 border-b-2 border-black ">
+				<GrDocumentTime className="self-center" />
+				<h2 className="text-xl font-semibold">Time Off</h2>
+			</div>
+			<div className="flex flex-row gap-8 mx-10">
 				{timeOffData.map((policy, index) => (
 					<div
-						className="w-full md:w-1/3 p-4 bg-[#f0f3f8] rounded-lg shadow-sm flex flex-col items-center"
+						className="w-full py-3 bg-[#F0F3F8] rounded-lg shadow-sm flex flex-col gap-1 items-center "
 						key={index}
 					>
-						<div className="text-center text-xl font-semibold">
+						<div className="text-center text-2xl font-semibold">
 							{policy.type}
 						</div>
-						<div className="text-3xl font-semibold">
+						<div className="flex flex-row items-center gap-2 text-3xl font-semibold">
+							{policy.icon}
 							{policy.daysAvailable}
+						</div>
+						<div className="text-center text-base font-semibold">
+							{policy.unitOfMeasurement}
 						</div>
 						<div className="text-center text-sm">
 							{policy.scheduledText}
